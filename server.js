c1ce5d6adc6ee41a956a6619e22bf91f9473aac7
jaste111 <jan.steimann@hhu.de>
2021-02-08 20:34:58 +0100

    Use ejs

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const ejs = require("ejs");
 const port = 3000;
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render('index');
+  res.render("index", { home: "Hi there", day: "1", "text": "Foo bar baz jfwieofjwioefjweioj" });
 });
 
 app.listen(port, function () {
