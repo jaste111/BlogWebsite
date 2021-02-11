04683ceb5c470f46bcffb6f394aa19de8d44ca03
jaste111 <jan.steimann@hhu.de>
2021-02-11 13:35:45 +0100

    Update class selector to hide readMore button

--- public/js/index.js
+++ public/js/index.js
@@ -2,5 +2,5 @@ function readMore(button){
     let buttonClass = button.className;
     let buttonId = buttonClass.split("-")[1];
     $(".p_" + buttonId).toggleClass("hide");
-    $("."+buttonClass).toggleClass("hide-button");
+    $(".readMore-"+buttonId).toggleClass("hide-button");
 }
