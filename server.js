5b5601cb1b1e6239ad2d1a6e925721a042167617
jaste111 <jan.steimann@hhu.de>
2021-02-16 16:03:41 +0100

    Remove dummy entries

--- server.js
+++ server.js
@@ -21,22 +21,6 @@ const postSchema = mongoose.Schema({
 // Crteate mongoose model
 const Post = mongoose.model("Post", postSchema);
 
-const day1 = {
-  day: "Day 1",
-  text:
-    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-};
-const day2 = {
-  day: "Day 2",
-  text:
-    "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
-};
-const home =
-  "Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.";
-
-let day = 3;
-let entries = [day1, day2];
-
 // Set templating engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -60,8 +44,8 @@ app.get("/", function (req, res) {
           entries: [],
         });
       } else {
-        const home = posts.find(element => element.title === "home");
-        const blogPosts = posts.filter(element => element.title !== "home");
+        const home = posts.find((element) => element.title === "home");
+        const blogPosts = posts.filter((element) => element.title !== "home");
         res.render("blog", { home: home.text, entries: blogPosts });
       }
     } else {
